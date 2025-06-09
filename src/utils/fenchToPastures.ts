c51992fb573779c5ae5dae4e4aecc3aeb7809b1b
chi <chi@taiwan.im>
2025-06-09 16:16:05 +0700

    fix:邏輯錯誤修復

--- src/utils/fenchToPastures.ts
+++ src/utils/fenchToPastures.ts
@@ -8,11 +8,12 @@ export const fenchToPastures = (fenceStoreSet: Set<string>) => {
     const x = Number.parseInt(match[0]);
     const y = Number.parseInt(match[1]);
     const type = match[2];
-    if (type == "V") return;
+    if (type == "V") continue;
     if (!map.has(x)) map.set(x, []);
     const array = map.get(x) ?? [];
     array.push(y);
   }
+
   const pastureSet = new Set<string>();
   map.forEach((yArray, x) => {
     const [start, end] = yArray.toSorted((a, b) => a - b);
